7ec00bcdb6c22cf597a5a65052e2e56e0e84c416
William Eckerleben <ing@williameckerleben.com>
2024-03-11 18:21:02 -0300

    Added route to serve swagger.json and updated Swagger UI configuration.
       - Added a new route '/swagger.json' to serve the Swagger JSON file.
       - Updated the configuration of Swagger UI to use this new route for the API documentation.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,30 @@
-from flask import Flask, jsonify
+import os
+
+from flask import Flask, jsonify, send_from_directory
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return jsonify(message="Hello, world!")
+
+
+@app.route('/swagger.json')
+def swagger_json():
+    return send_from_directory(os.getcwd(), 'swagger.json')
+
+
+SWAGGER_URL = '/docs'
+API_URL = '/swagger.json'
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "API Documentation"
+    }
+)
+
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
